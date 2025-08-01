9bf97893192c9a2a4bb368271ff198ae1be030e8
Amber Ainsley Garillos <agarillos73200@liceo.edu.ph>
2025-08-01 11:21:28 +0800

    lab-activty-2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main Branch!");
-    }
+        Scanner scanner = new Scanner(System.in);
+        int totalNumber = 0;
 
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+
+            totalNumber += scanner.nextInt();
+
+        }
+        System.out.println();
+        System.out.println("Total Number: " + totalNumber);
+        scanner.close();
+    }
 }
